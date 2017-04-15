d14c152165b36416a5e27bb687406d4b071784d6
Kevin Matthews <kmatthews@cyberdata.net>
2017-04-14 17:30:02 -0700

    Pack variables that are valid ascii.
    struct.pack() returns a string, so any values that are valid ascii are displayed as such.
    Use encode('hex') to make output consistent.

--- test_struct_packing.py
+++ test_struct_packing.py
@@ -6,8 +6,15 @@ def pack_one_variable():
 def pack_two_variables():
     return struct.pack("bB", -1, 1)
 
+def pack_ascii_variable():
+    return struct.pack("bB", -1, 0x42)
+
 def test_pack_one_variable():
     assert pack_one_variable() == '\xff'
 
 def test_pack_two_variables():
     assert pack_two_variables() == '\xff\x01'
+
+def test_pack_ascii_variable():
+    # Without encoding, the function returns: \xffB
+    assert pack_ascii_variable().encode('hex') == 'ff42'
