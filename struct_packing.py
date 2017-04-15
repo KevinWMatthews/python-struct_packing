75032f08ca985973bad8bbbbe41ede9d47d43b6e
Kevin Matthews <kmatthews@cyberdata.net>
2017-04-14 17:01:09 -0700

    Basic sanity check

--- struct_packing.py
+++ struct_packing.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 
+import struct
+
 print 'This is a demonstration of struct packing in Python'
+
+"""
+struct.pack(<format_string>, var1, var2, ...)
+
+Format characters:
+Format      C type          Size
+    c       char            1
+    b       signed char     1
+    B       unsigned char   1
+"""
+
+s = struct.pack("bB", -1, 1)
+print s
+print s.encode('hex')
