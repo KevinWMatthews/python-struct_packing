34feadb7c8013f194d782f8c1b75133828633ada
Kevin Matthews <kmatthews@cyberdata.net>
2017-04-14 17:34:33 -0700

    Test two byte variables, big endian

--- test_struct_packing.py
+++ test_struct_packing.py
@@ -9,6 +9,9 @@ def pack_two_variables():
 def pack_ascii_variable():
     return struct.pack("bB", -1, 0x42)
 
+def pack_two_byte_variables_big_endian():
+    return struct.pack(">hH", -1, 1)
+
 def test_pack_one_variable():
     assert pack_one_variable() == '\xff'
 
@@ -18,3 +21,7 @@ def test_pack_two_variables():
 def test_pack_ascii_variable():
     # Without encoding, the function returns: \xffB
     assert pack_ascii_variable().encode('hex') == 'ff42'
+
+def test_pack_two_byte_variables_big_endian():
+    # Endian-ness comes into play: 1 can be represented as 0001 or 0100.
+    assert pack_two_byte_variables_big_endian().encode('hex') == 'ffff0001'
