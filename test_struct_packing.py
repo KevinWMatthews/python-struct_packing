7b7501f00e58a0542aba1490721a7c1260d20059
Kevin Matthews <kmatthews@cyberdata.net>
2017-04-14 17:14:53 -0700

    Add second test - two variables

--- test_struct_packing.py
+++ test_struct_packing.py
@@ -3,5 +3,11 @@ import struct
 def pack_one_variable():
     return struct.pack("b", -1)
 
+def pack_two_variables():
+    return struct.pack("bB", -1, 1)
+
 def test_pack_one_variable():
     assert pack_one_variable() == '\xff'
+
+def test_pack_two_variables():
+    assert pack_two_variables() == '\xff\x01'
